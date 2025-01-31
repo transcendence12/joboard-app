2d43905ddbee506dd1ebd6e67424f2705b473cd3
transcendence12 <gosiek.krawczuk@gmail.com>
2025-01-31 13:40:29 +0100

    fix: fix font familly

--- src/components/OffersContainer/OffersContainer.jsx
+++ src/components/OffersContainer/OffersContainer.jsx
@@ -3,5 +3,7 @@ import * as styles from './OffersContainer.module.scss';
 import { OffersList } from './OffersList/OffersList';
 
 export const OffersContainer = (props) => (
-   <div className={styles.container}><OffersList /></div>
-)
+   <div className={styles.container}>
+      <OffersList />
+   </div>
+);
